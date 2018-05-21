95230645ff186f4152c048ef7db64281d7773738
Jason Roundtree <jasontree123@gmail.com>
2018-05-20 17:55:38 -0700

    Basic rendering of characters and partially functional pagination

---
+++ index.js
@@ -0,0 +1,76 @@
+'use strict'
+
+const RICK_AND_MORTY_CHARACTER_URL = 'https://rickandmortyapi.com/api/character'
+let dataStore
+let characters
+let currentPageNumber
+let prevButton = document.getElementById('previous')
+let nextButton = document.getElementById('next')
+
+function getCharacterDataFromAPI(URL) {
+    fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            dataStore = data
+            characters = data.results 
+            handleData(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+ 
+function handleData() {
+    if (dataStore.info.prev === "") {
+        prevButton.style.display = 'none'
+    } else {
+        prevButton.style.display = 'inline'
+    }
+    if (dataStore.info.next === dataStore.info.pages - 1) {
+        nextButton.style.display = 'none'
+    } else {
+        nextButton.style.display = 'inline'
+    }
+    renderData(characters)
+}
+
+function renderData(data) {
+    const htmlBody = document.getElementById('content')
+    return characters.map(character => {
+        htmlBody.innerHTML += 
+            `<div>
+                <img src=${character.image} />
+                <p>${character.name}</p>
+                <p>${character.species}</p>
+                <p>${character.status}</p>
+                <p>${character.gender}</p>
+            </div>`
+    })
+}
+
+function bindPrevPageButton() {
+    prevButton.addEventListener('click', prevPage)
+}
+
+function bindNextPageButton() {
+    nextButton.addEventListener('click', nextPage)
+}
+
+function prevPage() {
+    console.log('url: ', dataStore.info.prev)
+    getCharacterDataFromAPI(dataStore.info.prev)
+}
+
+function nextPage() {
+    console.log('url: ', dataStore.info.next)
+    getCharacterDataFromAPI(dataStore.info.next)
+}
+
+function setup() {
+    bindPrevPageButton()
+    bindNextPageButton()
+    getCharacterDataFromAPI(RICK_AND_MORTY_BASE_URL)
+}
+
+setup()
